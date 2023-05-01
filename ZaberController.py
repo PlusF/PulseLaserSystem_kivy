dcfb17404fcc4414aebf206342711e4e0acab0e0
LabPC <kaneda@photon.t.u-tokyo.ac.jp>
2023-05-01 15:06:50 +0900

    add base function for new developers

--- ZaberController.py
+++ ZaberController.py
@@ -1,2 +1,25 @@
 from zaber_motion import Units, Library, LogOutputMode
 from zaber_motion.binary import Connection
+
+
+# メモ：開発環境（自分のPC）で動作確認できるようにしておく
+class ZaberController:
+    def __init__(self):
+        # TODO: IMPLEMENT ME
+        pass
+
+    def move_top(self):
+        # TODO: IMPLEMENT ME
+        pass
+
+    def move_bottom(self):
+        # TODO: IMPLEMENT ME
+        pass
+
+    def move_left(self):
+        # TODO: IMPLEMENT ME
+        pass
+
+    def move_right(self):
+        # TODO: IMPLEMENT ME
+        pass
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+# メモ：ZaberControllerクラスをインポート
 
 
 class MainWindow(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # TODO: IMPLEMENT ME
+        # メモ：ZaberControllerクラスのインスタンスを作成
+
+    def move_top(self):
+        # TODO: IMPLEMENT ME
+        # メモ：ZaberControllerインスタンスのmove_top関数を呼び出す
+        pass
+
+    def move_bottom(self):
+        # TODO: IMPLEMENT ME
+        pass
+
+    def move_left(self):
+        # TODO: IMPLEMENT ME
+        pass
+
+    def move_right(self):
+        # TODO: IMPLEMENT ME
+        pass
 
 
 class MainApp(App):
