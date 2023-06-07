c45175936b63c1af03bdc40c7f5f9724573ad774
unknown <nishida@photon.t.u-tokyo.ac.jp>
2023-06-07 21:10:16 +0900

    placed all UI

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.base import ExceptionManager, ExceptionHandler
 # メモ：ZaberControllerクラスをインポート
 
-Window.size = (500, 1000)
+Window.size = (350, 700)
 
 class MainWindow(BoxLayout):
+    delta_x = NumericProperty(100)
+    delta_y = NumericProperty(50)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
         # TODO: IMPLEMENT ME
         # メモ：ZaberControllerクラスのインスタンスを作成
 
@@ -32,11 +37,18 @@ class MainWindow(BoxLayout):
         print(text)
 
 
+
 class MainApp(App):
     def build(self):
         window = MainWindow()
         return window
 
+class CrashHandler(ExceptionHandler):
+    def handle_exception(self, inst):
+        print(inst)
+        return ExceptionManager.PASS
+
+ExceptionManager.add_handler(CrashHandler())
 
 if __name__ == '__main__':
     MainApp().run()
