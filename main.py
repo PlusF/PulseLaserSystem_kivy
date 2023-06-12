55e3b28c70c25d34461163a36d4d5dd4e1815f20
hodaka2465 <nishimura-hodaka2465@g.ecc.u-tokyo.ac.jp>
2023-06-12 17:12:09 +0900

    'new'

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 import serial
 from kivy.app import App
 from kivy.uix.widget import Widget
-<<<<<<< HEAD
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
-from kivy.properties import NumericProperty
-from kivy.base import ExceptionManager, ExceptionHandler
-from kivy.clock import Clock
-import time
-
-from zaber_motion import Units, Library, LogOutputMode
-from zaber_motion.binary import Connection, Device
-
-=======
 from kivy.core.window import Window, Clock
 from kivy.properties import NumericProperty
+from ConfigLoader import ConfigLoader
+from kivy.base import ExceptionManager, ExceptionHandler
+from kivy.clock import Clock
 from PulseLaserController import PulseLaserController
->>>>>>> 2ad1dfd67d7d71d37799313f92f37d94273d51cb
-# メモ：ZaberControllerクラスをインポート
+import time
+from zaber_motion import Units, Library, LogOutputMode
+from zaber_motion.binary import Connection, Device
 from ZaberController import ZaberController
-# from ds102controller import MySerial, DS102Controller
-# from PulseLaserController import PulseLaserController
-# from ConfigLoader import ConfigLoader
-# from CommandWindow import CommandWindow
-#from PulseLaserController import PulseLaserController
 
 Window.size = (350, 700)
 
-<<<<<<< HEAD
+def control_auto_emission(func):
+    def wrapper(self, *args, **kwargs):
+        auto_on = self.ids.toggle_auto_emit.state == 'down'
+        if auto_on:
+            self.emit_laser()
+        ret = func(*args, **kwargs)
+        return ret
+    return wrapper
+
 class MainWindow(BoxLayout):
+    pos_x = NumericProperty(0, force_dispatch=True)
+    pos_y = NumericProperty(0, force_dispatch=True)
     delta_x = NumericProperty(100)
     delta_y = NumericProperty(50)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # TODO: IMPLEMENT ME
+        Window.bind(on_request_close=self.quit)
+
+
+        self.cl = ConfigLoader('./config.json')
+
+        self.freq: int = 100
+        self.speed: float = 100.0
+        #self.ser_laser = serial.Serial(port=self.cl.port_laser, baudrate=self.cl.baudrate_laser)
+        #self.laser = PulseLaserController(self.ser_laser)　レーザー装置につないでないので保留
         # メモ：ZaberControllerクラスのインスタンスを作成
         self.Zaberhandler = ZaberController("COM3")
+        self.stage = None
+       # Clock.schedule_interval(self.update_position, 0.1)  # 0.1秒ごとに位置を更新．エラーが起きたので保留
 
-#ステージ止める
-    def stop_all(self):
-        self.Zaberhandler.stop_all()
-#ステージ移動（速度指定)
+    def update_position(self):
+        self.pos_x, self.pos_y = self.stage.get_position()
+        pass
+
     def move_top(self):
         self.Zaberhandler.vel = float(self.ids.speed_input.text)
         self.Zaberhandler.move_top(float(self.ids.speed_input.text))
@@ -58,151 +67,28 @@ class MainWindow(BoxLayout):
 
     def move_right(self):
         self.Zaberhandler.vel = float(self.ids.speed_input.text)
-        #print(self.ids.speed_input.text)
         self.Zaberhandler.move_right(float(self.ids.speed_input.text))
         pass
 
-    def print_position(self):
-        print(f'x position : {self.Zaberhandler.device_x.get_position(unit=self.unit_pos)}')
-        print(f'y position : {self.Zaberhandler.device_y.get_position(unit=self.unit_pos)}')
-        time.sleep(1)
-        pass
-
-# ステージ移動（変位指定)
     def move_rect(self):
         self.Zaberhandler.vel = float(self.ids.speed_input.text)
         self.Zaberhandler.move_right()
         time.sleep(float(self.ids.deltaX.text) / float(self.ids.speed_input.text))
-        self.Zaberhandler.stop()
-        # self.Zaberhandler.y_relative_position = 0
-        # self.Zaberhandler.move_relative()
-        # self.Zaberhandler.x_relative_position = 0
-        # self.Zaberhandler.y_relative_position = int(self.ids.deltaY.text)
-        # self.Zaberhandler.move_relative()
-        # self.Zaberhandler.x_relative_position = -int(self.ids.deltaX.text)
-        # self.Zaberhandler.y_relative_position = 0
-        # self.Zaberhandler.move_relative()
+        self.Zaberhandler.stop_all()
         # self.Zaberhandler.x_relative_position = 0
         # self.Zaberhandler.y_relative_position = -int(self.ids.deltaY.text)
         # self.Zaberhandler.move_relative()
         pass
-
+        #
     def move_line(self):
         self.x_relative_position = self.ids.deltaX.text
         self.y_relative_position = self.ids.deltaY.text
         move_relative(self)
         pass
-
+        #
     #set origin
     def set_org(self):
         self.Zaberhandler.x_relative_position = 0
-        pass
-
-    #レーザー射出
-
-    # def emit(self):
-   #      frq = self.frq.get()
-   #      if not 16 <= frq <= 10000:
-   #          print('Frequency must be 16~10000 Hz.')
-   #      if toggle_manual_emit.state == "normal"
-   #          self.laser.set_frq(frq)
-   #          print('Emit')
-   #  def stop_laser(self):
-   #          self.laser.stop()
-   #          print('Stop laser')
-
-    def check(self,text):
-        print(text)
-
-#描画
-class MainApp(App):
-    def build(self):
-        window = MainWindow()
-        return window
-
-
-
-class CrashHandler(ExceptionHandler):
-    def handle_exception(self, inst):
-        print(inst)
-        return ExceptionManager.PASS
-ExceptionManager.add_handler(CrashHandler())
-
-
-if __name__ == '__main__':
-    MainApp().run()
-
-
-
-
-
-
-
-
-
-
-
-import serial
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.core.window import Window, Clock
-from kivy.properties import NumericProperty
-from ConfigLoader import ConfigLoader
-from PulseLaserController import PulseLaserController
-# メモ：ZaberControllerクラスをインポート
-
-
-def control_auto_emission(func):
-    def wrapper(self, *args, **kwargs):
-        auto_on = self.ids.toggle_auto_emit.state == 'down'
-        if auto_on:
-            self.emit_laser()
-        ret = func(*args, **kwargs)
-        return ret
-    return wrapper
-
-
-class MainWindow(Widget):
-    pos_x = NumericProperty(0, force_dispatch=True)
-    pos_y = NumericProperty(0, force_dispatch=True)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        Window.bind(on_request_close=self.quit)
-
-
-        self.cl = ConfigLoader('./config.json')
-
-        self.freq: int = 100
-        self.speed: float = 100.0
-        self.ser_laser = serial.Serial(port=self.cl.port_laser, baudrate=self.cl.baudrate_laser)
-        self.laser = PulseLaserController(self.ser_laser)
-        # メモ：ZaberControllerクラスのインスタンスを作成
-        self.stage = None
-        Clock.schedule_interval(self.update_position, 0.1)  # 0.1秒ごとに位置を更新．
-
-    def update_position(self):
-        self.pos_x, self.pos_y = self.stage.get_position()
-
-    @control_auto_emission
-    def move_top(self):
-        # TODO: IMPLEMENT ME
-        # メモ：ZaberControllerインスタンスのmove_top関数を呼び出す
-        pass
-
-    @control_auto_emission
-    def move_bottom(self):
-        # TODO: IMPLEMENT ME
-        pass
-
-    @control_auto_emission
-    def move_left(self):
-        # TODO: IMPLEMENT ME
-        pass
-
-    @control_auto_emission
-    def move_right(self):
-        # TODO: IMPLEMENT ME
         pass
 
     def stop_moving(self):
@@ -251,6 +137,7 @@ class MainWindow(Widget):
 
     def quit(self):
         self.ser_laser.close()
+        self.Zaberhandler.quit()
 
 
 class MainApp(App):
@@ -258,6 +145,15 @@ class MainApp(App):
         window = MainWindow()
         return window
 
+    #必要になるかも
+    # class CrashHandler(ExceptionHandler):
+    #     def handle_exception(self, inst):
+    #         print(inst)
+    #         return ExceptionManager.PASS
+    #
+    # ExceptionManager.add_handler(CrashHandler())
+
 
 if __name__ == '__main__':
     MainApp().run()
+
