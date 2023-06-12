4277b303d2b142beaafda331876a758ab4c493c5
hodaka2465 <nishimura-hodaka2465@g.ecc.u-tokyo.ac.jp>
2023-06-12 15:29:41 +0900

    'new'

--- ZaberController.py
+++ ZaberController.py
@@ -1,25 +1,128 @@
+import time
+import asyncio
 from zaber_motion import Units, Library, LogOutputMode
-from zaber_motion.binary import Connection
+from zaber_motion.binary import Connection, Device
 
-
+# 接続後の初回操作時，positionが[25400, 25400](最大値)になってしまう　電源を落とす直前に[25400, 25400]に移動しておくことで一応対処可能(0.01μmオーダーの誤差はある)
+# システム上でpositionの値域を超えて動かそうとしても止まってしまう
+# システム上は問題ないが現実の最小値(0)まで到達すると座標がリセットされる
+# positonの値域は0~25400
 # メモ：開発環境（自分のPC）で動作確認できるようにしておく
+
 class ZaberController:
-    def __init__(self):
-        # TODO: IMPLEMENT ME
-        pass
+    def __init__(self, port):
+        self.unit_pos = Units.LENGTH_MICROMETRES
+        self.unit_vel = Units.VELOCITY_MICROMETRES_PER_SECOND
 
-    def move_top(self):
-        # TODO: IMPLEMENT ME
-        pass
+        self.default_position = [25400, 25400]
 
-    def move_bottom(self):
-        # TODO: IMPLEMENT ME
-        pass
+        self.port = port
+        self.connection = Connection.open_serial_port(self.port)
+        # 引数[0]がパソコンに直接つながれているアクチュエータ，引数[1]が連結されたもう一つのアクチュエータ
+        self.device_x = self.connection.detect_devices()[0]
+        # print(self.device_x)
+        self.device_y = self.connection.detect_devices()[1]
+        # print(self.device_y)
 
-    def move_left(self):
-        # TODO: IMPLEMENT ME
-        pass
+    def move_top(self, vel):
+        self.device_y.move_velocity(vel, unit=self.unit_vel)
 
-    def move_right(self):
-        # TODO: IMPLEMENT ME
-        pass
+    def move_bottom(self, vel):
+        self.device_y.move_velocity(-vel, unit=self.unit_vel)
+
+    def move_left(self, vel):
+        self.device_x.move_velocity(-vel, unit=self.unit_vel)
+
+    def move_right(self, vel):
+        self.device_x.move_velocity(vel, unit=self.unit_vel)
+
+    async def move_absolute(self, position_x, position_y):
+        #x,yが同時に動く
+        if (0 <= position_x <= 25400) and (0 <= position_y <= 25400):
+            await asyncio.gather(
+                self.device_x.move_absolute_async(position_x, unit=self.unit_pos),
+                self.device_y.move_absolute_async(position_y, unit=self.unit_pos)
+            )
+            return True
+        else:
+            print('move_absolute : positions are out of range')
+            return False
+
+    async def move_relative(self, position_x, position_y):
+        #x,yが同時に動く
+        position_list = self.get_position_all()
+        x = position_list[0] + position_x #移動後の座標
+        y = position_list[1] + position_y
+        if (0 <= x <= 25400) and (0 <= y <= 25400):
+            await asyncio.gather(
+                self.device_x.move_relative_async(position_x, unit=self.unit_pos),
+                self.device_y.move_relative_async(position_y, unit=self.unit_pos)
+            )
+            return True
+        else:
+            print('move_relative : positions are out of range')
+            return False
+
+    async def move_default(self):
+        await self.move_absolute(self.default_position[0], self.default_position[1])
+
+    def get_position_x(self):
+        return self.device_x.get_position(unit=self.unit_pos)
+
+    def get_position_y(self):
+        return self.device_y.get_position(unit=self.unit_pos)
+
+    #x,y座標をリストにして返す
+    def get_position_all(self):
+        x = self.get_position_x()
+        y = self.get_position_y()
+        return [x, y]
+
+    def stop_x(self):
+        self.device_x.stop()
+
+    def stop_y(self):
+        self.device_y.stop()
+
+    def stop_all(self):
+        self.stop_x()
+        self.stop_y()
+
+    def quit(self):
+        self.connection.close()
+
+async def test(self):
+    #各種関数の動作確認用
+
+    print('position before moving')
+    print(f'x position : {self.get_position_x()}  y position : {self.get_position_y()}')
+
+    # await self.move_absolute(20000, 20000)
+    # await self.move_relative(1000, 1000)
+    await self.move_default()
+    # self.move_right(1000)
+    # self.move_left(1000)
+    # self.move_top(1000)
+    # self.move_bottom(1000)
+
+    input('enterでstop') #応急処置
+    self.stop_all()
+
+    print('position after moving')
+    print(f'x position : {self.get_position_x()}  y position : {self.get_position_y()}')
+
+def main():
+    PORT_NUM = "COM3"  # check
+    z = ZaberController(PORT_NUM)
+
+    asyncio.run(test(z))
+
+    # loop = asyncio.get_event_loop() #こちらで実行すると次の警告が出る：DeprecationWarning: There is no current event loop
+    # tasks = asyncio.gather(z.test())
+    # loop.run_until_complete(tasks)
+
+    z.quit()
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,39 +1,125 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.base import ExceptionManager, ExceptionHandler
+from kivy.clock import Clock
+import time
+
+from zaber_motion import Units, Library, LogOutputMode
+from zaber_motion.binary import Connection, Device
+
 # メモ：ZaberControllerクラスをインポート
+from ZaberController import ZaberController
+# from ds102controller import MySerial, DS102Controller
+# from PulseLaserController import PulseLaserController
+# from ConfigLoader import ConfigLoader
+# from CommandWindow import CommandWindow
+#from PulseLaserController import PulseLaserController
 
+Window.size = (350, 700)
 
-class MainWindow(Widget):
+class MainWindow(BoxLayout):
+    delta_x = NumericProperty(100)
+    delta_y = NumericProperty(50)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # TODO: IMPLEMENT ME
         # メモ：ZaberControllerクラスのインスタンスを作成
+        self.Zaberhandler = ZaberController("COM3")
 
+#ステージ止める
+    def stop_all(self):
+        self.Zaberhandler.stop_all()
+#ステージ移動（速度指定)
     def move_top(self):
-        # TODO: IMPLEMENT ME
-        #self.move('y', 1, event)
-        # メモ：ZaberControllerインスタンスのmove_top関数を呼び出す
+        self.Zaberhandler.vel = float(self.ids.speed_input.text)
+        self.Zaberhandler.move_top(float(self.ids.speed_input.text))
         pass
 
     def move_bottom(self):
-        # TODO: IMPLEMENT ME
+        self.Zaberhandler.vel = float(self.ids.speed_input.text)
+        self.Zaberhandler.move_bottom(float(self.ids.speed_input.text))
         pass
 
     def move_left(self):
-        # TODO: IMPLEMENT ME
+        self.Zaberhandler.vel = float(self.ids.speed_input.text)
+        self.Zaberhandler.move_left(float(self.ids.speed_input.text))
         pass
 
     def move_right(self):
-        # TODO: IMPLEMENT ME
+        self.Zaberhandler.vel = float(self.ids.speed_input.text)
+        #print(self.ids.speed_input.text)
+        self.Zaberhandler.move_right(float(self.ids.speed_input.text))
         pass
 
+    def print_position(self):
+        print(f'x position : {self.Zaberhandler.device_x.get_position(unit=self.unit_pos)}')
+        print(f'y position : {self.Zaberhandler.device_y.get_position(unit=self.unit_pos)}')
+        time.sleep(1)
+        pass
 
+# ステージ移動（変位指定)
+    def move_rect(self):
+        self.Zaberhandler.vel = float(self.ids.speed_input.text)
+        self.Zaberhandler.move_right()
+        time.sleep(float(self.ids.deltaX.text) / float(self.ids.speed_input.text))
+        self.Zaberhandler.stop()
+        # self.Zaberhandler.y_relative_position = 0
+        # self.Zaberhandler.move_relative()
+        # self.Zaberhandler.x_relative_position = 0
+        # self.Zaberhandler.y_relative_position = int(self.ids.deltaY.text)
+        # self.Zaberhandler.move_relative()
+        # self.Zaberhandler.x_relative_position = -int(self.ids.deltaX.text)
+        # self.Zaberhandler.y_relative_position = 0
+        # self.Zaberhandler.move_relative()
+        # self.Zaberhandler.x_relative_position = 0
+        # self.Zaberhandler.y_relative_position = -int(self.ids.deltaY.text)
+        # self.Zaberhandler.move_relative()
+        pass
+
+    def move_line(self):
+        self.x_relative_position = self.ids.deltaX.text
+        self.y_relative_position = self.ids.deltaY.text
+        move_relative(self)
+        pass
+
+    #set origin
+    def set_org(self):
+        self.Zaberhandler.x_relative_position = 0
+        pass
+
+    #レーザー射出
+
+    # def emit(self):
+   #      frq = self.frq.get()
+   #      if not 16 <= frq <= 10000:
+   #          print('Frequency must be 16~10000 Hz.')
+   #      if toggle_manual_emit.state == "normal"
+   #          self.laser.set_frq(frq)
+   #          print('Emit')
+   #  def stop_laser(self):
+   #          self.laser.stop()
+   #          print('Stop laser')
+
+    def check(self,text):
+        print(text)
+
+#描画
 class MainApp(App):
     def build(self):
         window = MainWindow()
         return window
 
-    #def move(self, axis: str, direction: int):
+
+
+class CrashHandler(ExceptionHandler):
+    def handle_exception(self, inst):
+        print(inst)
+        return ExceptionManager.PASS
+ExceptionManager.add_handler(CrashHandler())
+
 
 if __name__ == '__main__':
     MainApp().run()
