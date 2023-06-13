376ca98f0a315e2df46c731f30523d89495b090d
penguin-web <penguinpenpen753@gmail.com>
2023-06-13 17:20:20 +0900

    update main.kv main.kv source

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.properties import NumericProperty
 from kivy.base import ExceptionManager, ExceptionHandler
+import math
 # メモ：ZaberControllerクラスをインポート
 
-Window.size = (350, 700)
+
+
+class CrashHandler(ExceptionHandler):
+    def handle_exception(self, inst):
+        print(inst)
+        return ExceptionManager.PASS
 
 class MainWindow(BoxLayout):
     delta_x = NumericProperty(100)
     delta_y = NumericProperty(50)
+    Window.size = (350, 700)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
         # TODO: IMPLEMENT ME
         # メモ：ZaberControllerクラスのインスタンスを作成
 
@@ -33,8 +39,72 @@ class MainWindow(BoxLayout):
     def move_right(self):
         # TODO: IMPLEMENT ME
         pass
+
     def check(self,text):
         print(text)
+
+    def check_freq(self,text):
+        num = int(text)
+        if num < 16 :
+            num = 16
+        elif num > 10000:
+            num = 10000
+        
+        return str(num)
+    
+    def check_speed(self,text):
+        num = int(text)
+        if num < 1 :
+            num = 1
+        elif num > 1000:
+            num = 1000
+        
+        return str(num)
+    
+    def set_freq_from_slider(self,value):
+        freq = 0
+        if value==1:
+            freq = 16
+        elif value==2:
+            freq = 50
+        elif value==3:
+            freq = 100
+        elif value==4:
+            freq = 500
+        elif value==5:
+            freq = 1000
+        elif value==6:
+            freq = 5000
+        elif value==7:
+            freq = 10000
+        return freq
+    
+    def set_speed_from_slider(self, value):
+        speed = 0
+        if value==1:
+            speed = 1
+        elif value==2:
+            speed = 5
+        elif value==3:
+            speed = 10
+        elif value==4:
+            speed = 50
+        elif value==5:
+            speed = 100
+        elif value==6:
+            speed = 500
+        elif value==7:
+            speed = 1000
+        return speed
+    
+    def start_program_mode(self):
+        pass
+
+
+    ExceptionManager.add_handler(CrashHandler())
+
+
+
 
 
 
@@ -43,12 +113,10 @@ class MainApp(App):
         window = MainWindow()
         return window
 
-class CrashHandler(ExceptionHandler):
-    def handle_exception(self, inst):
-        print(inst)
-        return ExceptionManager.PASS
 
-ExceptionManager.add_handler(CrashHandler())
+
+
 
 if __name__ == '__main__':
     MainApp().run()
+    
